b2821fc5e94cab71a9823d44096bd22f36def824
bimorhfdz <bimorhfdz@gmail.com>
2023-11-07 15:30:36 +0700

    v 1-7

---
+++ router/users.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router =express.Router()
+  
+
+let users = [
+    {id : 1, nama : "Bimo", email : "bimorhfdz@Gmail.com"},
+    {id : 2, nama : "Adit", email : "aditya@Gmail.com"},
+    {id : 3, nama : "Zarine", email : "zarine@Gmail.com"}
+]
+
+
+  router.get('/users', (req, res) => {
+    res.json (users)
+  })
+  
+  router.post('/user', (req, res) => {
+    users.push(req.body)
+    res.json(users)
+  })
+  
+  router.put('/user/:id', (req, res) => {
+    const id = req.params.id
+    users.filter(user => {
+        if(user.id == id){
+            user.nama = req.body.nama
+            user.email = req.body.email
+            return user
+        }
+    })
+    res.json(users)
+  })
+  
+  router.delete('/user/:id', (req, res) => {
+    const id = req.params.id
+    users = users.filter(user => user.id != id)
+
+    res.json(users)
+  })
+  
+
+  module.exports = router
